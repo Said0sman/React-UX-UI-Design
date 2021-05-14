29e97d554281e6c46b316f9d54f7d1095446dc74
Said Osman <said_henry@hotmail.com>
2021-05-14 17:19:47 +0200

    UX/UI
    React UX/UI Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>App</h1>
+      <h1>This is a main headline</h1>
+      <h2>This is a main Secondary headline</h2>
+      <h3>This is a Third headline or main label</h3>
+      <p className="primary_text">This is normal text</p>
+      <p className="secondary_text">This is for secondary information</p>
+      <p className="control_labels">
+        This is for control lables and timestamps
+      </p>
+      <button className="primary_button">Click here</button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './utils/api/global/css/Global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
